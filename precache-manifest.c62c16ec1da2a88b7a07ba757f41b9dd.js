d6f424255a11da3b5dc497d1cb91d29685446cfa
manish <manish01.it@gmail.com>
2020-11-29 23:06:56 +0530

    Updates

---
+++ precache-manifest.c62c16ec1da2a88b7a07ba757f41b9dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa53748ed1bd4d7df003e4e7bac8554",
+    "url": "/manishbisht-1/React-Website.git/index.html"
+  },
+  {
+    "revision": "7aa3e57a960b965d77e4",
+    "url": "/manishbisht-1/React-Website.git/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8c5e50ac0c87531bfcfd",
+    "url": "/manishbisht-1/React-Website.git/static/css/main.ddf35e70.chunk.css"
+  },
+  {
+    "revision": "7aa3e57a960b965d77e4",
+    "url": "/manishbisht-1/React-Website.git/static/js/2.50856dbd.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/manishbisht-1/React-Website.git/static/js/2.50856dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5e50ac0c87531bfcfd",
+    "url": "/manishbisht-1/React-Website.git/static/js/main.ca505893.chunk.js"
+  },
+  {
+    "revision": "7cfd9a8f7a2ff149f6bb",
+    "url": "/manishbisht-1/React-Website.git/static/js/runtime-main.a6488276.js"
+  },
+  {
+    "revision": "bf5f649b2c6e7cd7e35b7b415e3cd092",
+    "url": "/manishbisht-1/React-Website.git/static/media/Main.bf5f649b.jpg"
+  },
+  {
+    "revision": "75610765283ec28bd320f9d2bf6c9004",
+    "url": "/manishbisht-1/React-Website.git/static/media/images (1)1.75610765.jpg"
+  }
+]);
